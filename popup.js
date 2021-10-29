220876213575969c5dd3ef1335465a8b8db39166
HyruleHer0 <hyrule.ck@gmail.com>
2021-10-29 07:54:08 -0500

    More work & new icons

--- popup.js
+++ popup.js
@@ -2,41 +2,109 @@
 
 
 var rootNode;
+var parentId;
+
+// Elements
 const bookmarkDiv = document.getElementById('bookmarks');
-bookmarkDiv.addEventListener('click', clickFolder);
+const headerElem = document.querySelector('header');
+const upBtn = document.getElementById('up-btn');
+
+assignHandlers();
 
 chrome.bookmarks.getTree(function (allNodes) {
     rootNode = allNodes[0];
-    displayChildren(rootNode);
+    setActiveNode(rootNode);
 });
 
-function displayChildren(node) {
-    if (!!node && !!node.children & !!node.children.length) {
+function setActiveNode(node) {
+    if (!!node.children & !!node.children.length) {
+        parentId = node.parentId;
+        bookmarkDiv.textContent = '';
+
+        // chrome.storage.sync.set({'last-fetched': node.id});
+
         node.children.forEach(child => {
-            if (!node.hasOwnProperty('url')) { // Check that it's a folder
-                const div = document.createElement('div');
-                div.classList.add('bookmark-folder');
-                div.textContent = child.title;
-                div.dataset.id = child.id;
+            if (!child.hasOwnProperty('url')) { // Check that it's a folder
+                const div = buildHTMLForFolder(child);
                 bookmarkDiv.append(div);
             }
         });
     }
-    else {
-        bookmarkDiv.innerText = "Nothing to display";
+
+    console.log(parentId);
+    upBtn.classList.toggle('hide', !parentId && parentId !== 0);
+}
+
+function setActiveNodebyId(id) {
+    chrome.bookmarks.getSubTree(id, result => {
+        if (!!result && result.length) {
+            setActiveNode(result[0]);
+        }
+    });
+}
+
+function buildHTMLForFolder(node) {
+    const div = document.createElement('div');
+    div.title = node.title;
+    div.classList.add('bookmark-folder');
+    div.dataset.id = node.id;
+
+    if (isEndOfPath(node)) {
+        div.classList.add('ghosted');
     }
+
+    let bmCount = 0, folderCount = 0;
+    node.children.forEach(grandchild => {
+        if (grandchild.hasOwnProperty('url')) {
+            bmCount++;
+        }
+        else {
+            folderCount++;
+        }
+    });
+
+    const title = document.createElement('span');
+    title.textContent = node.title;
+    title.classList.add('title');
+
+    const childCount = document.createElement('span');
+    childCount.textContent = `${folderCount} / ${bmCount}`;
+    childCount.classList.add('child-count');
+
+    div.appendChild(title);
+    div.appendChild(childCount);
+
+    return div;
+}
+
+function isEndOfPath(node) {
+    return !node.children || node.children.length === 0 || node.children.every(n => {
+        n.hasOwnProperty('url')
+    });
 }
 
 function clickFolder(e) {
     const folder = e.target.closest('.bookmark-folder');
-    if (!!folder) {
-        chrome.bookmarks.getChildren(folder.dataset.id, result => {
-            if (!!result) { 
-                displayChildren(result[0]);
-            }
-            else {
-                console.error(`Failed to fetch bookmark node id ${folder.dataset.id}. ${result}`);
-            }
-        });
+    if (!!folder && !folder.classList.contains('ghosted')) {
+        setActiveNodebyId(folder.dataset.id);
     }
+}
+
+function clickHeader(e) {
+    const btn = e.target.closest('button');
+    console.log("header clicked");
+    console.log("btn: ", btn);
+    if (!!btn){
+        switch (btn.id) {
+            case ('up-btn') :
+                console.log("up clicked")
+                setActiveNodebyId(parentId);
+                break;
+        }
+    }
+}
+
+function assignHandlers() {
+    bookmarkDiv.addEventListener('click', clickFolder);
+    headerElem.addEventListener('click', clickHeader);
 }
